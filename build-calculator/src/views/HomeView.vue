e155ecfd5ddc4e1abbcdd87715514a2f0dc5f77d
yanshou517 <ra89517r@gmail.com>
2024-10-31 13:55:40 +0800

    [更新]移除無用router 新增標題

--- build-calculator/src/views/HomeView.vue
+++ build-calculator/src/views/HomeView.vue
@@ -1,46 +1,24 @@
 <template>
-  <div class="home">
+  <!-- <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <button @click="fetchGoogleSheet">測試按鈕</button>
-  </div>
-  <div class="total">
-    <!-- <p class="rows" v-for="(item, index) in processedData" :key="index">
-      {{ item.index }} - {{ item.itemName }} - {{ item.itemQuantityUnit }} -
-      {{ item.count }} - {{ item.price }}
-    </p> -->
-  </div>
-
-  <!-- <div class="container text-center">
-    <div class="row">
-      <div class="col">{{ item.itemName }}</div>
-      <div class="col">{{ item.itemQuantityUnit }}</div>
-      <div class="col">{{ item.index }}</div>
-      <div class="col">{{ item.count }}</div>
-      <div class="col">{{ item.price }}</div>
-    </div>
   </div> -->
 
-  <!-- <div class="container text-center">
-    <div class="row">
-      <div class="col" style="background-color: red">1</div>
-      <div class="col-7" style="background-color: blue">5.5m2電源迴路增設</div>
-      <div class="col" style="background-color: yellow">迴</div>
-      <div class="col" style="background-color: orange">1</div>
-      <div class="col-2" style="background-color: purple">NT$3,500</div>
-    </div>
-  </div> -->
+  <div>
+    <h1>統包大叔團隊 新成屋輕預算 線上報價試算</h1>
+  </div>
 
   <div class="container text-center">
     <div class="row" v-for="(item, index) in processedData" :key="index">
-      <div class="col border border-1">{{ item.index }}</div>
+      <div class="col-1 border border-1">{{ item.index }}</div>
       <div class="col-3 border border-1">{{ item.itemName }}</div>
-      <div class="col border border-1">{{ item.itemQuantityUnit }}</div>
+      <div class="col-1 border border-1">{{ item.itemQuantityUnit }}</div>
 
       <!-- 若欄位為數量，顯示下拉式選單；否則顯示文本 -->
       <div class="col border border-1">
         <template v-if="typeof item.count === 'number'">
           <select v-model="item.count" @change="updateTotalPrice(index)">
-            <option v-for="n in 10" :key="n" :value="n - 1">{{ n }}</option>
+            <option v-for="n in 10" :key="n" :value="n">{{ n }}</option>
           </select>
         </template>
         <template v-else>
@@ -48,7 +26,7 @@
         </template>
       </div>
 
-      <div class="col border border-1">
+      <div class="col-3 border border-1">
         {{
           typeof item.price === "number" && !isNaN(item.price)
             ? `NT$${item.price.toLocaleString()}元`
@@ -64,12 +42,12 @@
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
   data() {
     return {
@@ -88,7 +66,7 @@ export default {
         .then((data) => {
           console.log(data);
           this.processedData = []; // 清空已存在的資料
-          this.sheetDataProcess(data.values); // 使用 data.values 而非 data.value
+          this.sheetDataProcess(data.values);
         })
         .catch((error) => {
           console.error("Error:", error);
@@ -102,7 +80,6 @@ export default {
           index: element[0],
           itemName: element[1],
           itemQuantityUnit: element[2],
-          // count: isNaN(element[3]) ? element[3] : Number(element[3]),
           count: 0,
           price:
             typeof element[4] === "string" && element[4].includes("NT$")
@@ -112,21 +89,25 @@ export default {
         this.processedData.push(obj);
       });
     },
+
     // 增加 count
     incrementCount(index) {
       this.processedData[index].count++;
     },
+
     // 減少 count，確保 count 不會低於 0
     decrementCount(index) {
       if (this.processedData[index].count > 0) {
         this.processedData[index].count--;
       }
     },
+
     updateTotalPrice(index) {
       this.processedData[index].totalPrice =
         this.processedData[index].count * this.processedData[index].price;
     },
   },
+
   mounted() {
     this.fetchGoogleSheet();
   },
