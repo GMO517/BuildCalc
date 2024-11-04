c9e08b4485d5a02c3d23b26cd3e9ca9231b8eebe
yanshou517 <ra89517r@gmail.com>
2024-11-04 23:17:00 +0800

    [更新]初次切版 新增總價及備註

--- build-calculator/src/views/HomeView.vue
+++ build-calculator/src/views/HomeView.vue
@@ -1,27 +1,49 @@
 <template>
-  <div>
-    <h1>統包大叔團隊 新成屋輕預算 線上報價試算</h1>
-  </div>
+  <header>
+    <div>
+      <img :src="require('@/assets/logo/logo.png')" />
+      <h2>統包大叔團隊 新成屋輕預算 線上報價試算</h2>
+    </div>
+  </header>
 
   <div class="container text-center">
+    <div class="swiperClass">
+      <swiper
+        class="sampleSwiper"
+        :module="modules"
+        :pagination="{ clickable: true }"
+      >
+        <swiper-slide>
+          <img :src="require('@/assets/slider/20200926133419jnum2.jpg')" />
+        </swiper-slide>
+        <swiper-slide>
+          <img :src="require('@/assets/slider/20200926134016uz826.jpg')" />
+        </swiper-slide>
+      </swiper>
+    </div>
+    <div class="row">
+      <div class="col-1 border border-1">序號</div>
+      <div class="col-3 border border-1">工程項目</div>
+      <div class="col-1 border border-1">單位</div>
+      <div class="col border border-1">數量</div>
+      <div class="col-3 border border-1">單價</div>
+      <div class="col border border-1">總金額</div>
+    </div>
+
     <div class="row" v-for="(item, index) in processedData" :key="index">
       <div class="col-1 border border-1">{{ item.index }}</div>
       <div class="col-3 border border-1">{{ item.itemName }}</div>
       <div class="col-1 border border-1">{{ item.itemQuantityUnit }}</div>
 
-      <!-- 下拉式選單和自定義數字輸入 -->
       <div class="col border border-1">
-        <template v-if="index != 0">
-          <select
-            v-model="selectedCount[index]"
-            @change="updateTotalPrice(index)"
-            :disabled="isPriceInvalid(item.price)"
-          >
-            <option value="0">0</option>
-            <option v-for="n in 10" :key="n" :value="n">{{ n }}</option>
-          </select>
-        </template>
-        <template v-else> 數量 </template>
+        <select
+          v-model="selectedCount[index]"
+          @change="updateTotalPrice(index)"
+          :disabled="isPriceInvalid(item.price)"
+        >
+          <option value="0">0</option>
+          <option v-for="n in 10" :key="n" :value="n">{{ n }}</option>
+        </select>
       </div>
 
       <div class="col-3 border border-1">
@@ -32,21 +54,57 @@
         {{ itemPriceSumProcess(index, item.price) }}
       </div>
     </div>
-    <p>總計:</p>
-    <div class="col border border-1">NT${{ totalPrice.toLocaleString() }}</div>
+    
+    <div class="row">
+      <div class="col-5 border border-1">總計:</div>
+      <div class="col-7 border border-1">
+        NT${{ totalPrice.toLocaleString() }}
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-5 border border-1">
+        預估雜項總計(依裝修經驗約為5~10%↑):
+      </div>
+      <div class="col-7 border border-1">
+        NT${{ totalPriceWithExtra(minAddition).toLocaleString() }}~ NT${{
+          totalPriceWithExtra(maxAddition).toLocaleString()
+        }}
+      </div>
+    </div>
+
+    <div class="row note-content border border-2">
+      說明:此線上估價試算系統使用期限約為3天，裝修金額隨著個人需求會有些許調整。<br />
+      試算後有符合者，本公司收費3,000元(合作後可併入工程款)，以利安排場勘及施工前準備及進度表。<br />
+      匯款帳號:星展銀行(810)中壢分行 6709209238 達圓室內裝修工程有限公司
+    </div>
   </div>
 </template>
 
 <script>
+import "@/style/global.css";
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/pagination";
+
 export default {
-  name: "HomeView",
   data() {
     return {
+      modules: [Pagination],
       processedData: [],
-      selectedCount: [], // 用來存儲選擇的數量
-      customCount: [], // 用來存儲自定義的數量
+      selectedCount: [],
+      customCount: [],
+      minAddition: 5,
+      maxAddition: 10,
     };
   },
+
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+
   methods: {
     fetchGoogleSheet() {
       const apiKey = "AIzaSyCblqD68QVjdlMY5jN10euonBBGopL0J08";
@@ -91,13 +149,11 @@ export default {
     },
 
     updateSelect(index) {
-      // 當使用者輸入自定義數字時，同步更新選擇的數字
       this.selectedCount[index] = this.customCount[index];
       this.updateTotalPrice(index); // 更新總價
     },
 
     updateTotalPrice(index) {
-      // 如果使用者選擇了數量，更新總價
       const count = this.selectedCount[index] || this.customCount[index] || 0;
       this.processedData[index].count = Number(count);
       this.processedData[index].totalPrice =
@@ -112,14 +168,12 @@ export default {
       }
     },
 
-    //商品數量資料處理
     itemCountProcess(count) {
       if (typeof count === "number" && !isNaN(count))
         return `NT$${count.toLocaleString()}元`;
       else return count;
     },
 
-    // 項目總價處理
     itemPriceSumProcess(dataIndex, itemPrice) {
       if (itemPrice === undefined || dataIndex === undefined) {
         return "";
@@ -136,30 +190,32 @@ export default {
     },
 
     isPriceInvalid(price) {
-      // 判斷價格是否為 undefined、空字串或不包含任何數字
       return (
         price === undefined ||
         price === "" ||
         (typeof price === "string" && !/\d/.test(price))
       );
     },
+
+    totalPriceWithExtra(addition) {
+      return this.totalPrice * (1 + addition / 100);
+    },
   },
 
   computed: {
-    // 計算所有項目的總價
     totalPrice() {
-      return this.processedData.reduce((sum, item, index) => {
-        const count = this.selectedCount[index] || this.customCount[index] || 0;
-        return sum + (count * item.price || 0);
-      }, 0);
+      return (
+        this.processedData.reduce((sum, item, index) => {
+          const count =
+            this.selectedCount[index] || this.customCount[index] || 0;
+          return sum + (count * item.price || 0);
+        }, 0) || 0
+      );
     },
   },
+
   mounted() {
     this.fetchGoogleSheet();
   },
 };
 </script>
-
-<style scoped>
-/* 可以根據需要自定義樣式 */
-</style>
